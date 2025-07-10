8c5a49f911ff8b59f472baa6725b1ffbfcc58d15
Mahalakshmi P <mahalekshm.p1989@gmail.com>
2025-07-10 11:05:42 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Remove experimental.appDir
+  webpack: (config) => {
+    config.watchOptions = {
+      ignored: [
+        "**/.git/**",
+        "**/node_modules/**",
+        "**/C:/Users/USER/Application Data/**", // Ignore restricted Windows path
+      ],
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
